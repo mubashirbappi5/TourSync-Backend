c87bdf75d6639a8f000cb119f96489911a28c83c
mubashirbappi <mubashirbappi@gmail.com>
2026-02-15 11:42:41 +0600

    register api working

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
 
 import express, { Request, Response } from "express";
-
+import cors from "cors";
 
 import dotenv from "dotenv";
+import { userRoute } from "./app/modules/user/User.route";
 
 dotenv.config();
 
 
 const app = express();
 
+app.use(express.json())
+app.use(cors())
 
+
+app.use("/api/v1/user",userRoute)
 app.get("/",(req:Request, res:Response)=>{
     res.send("Welcome to TourSync API");
 })
 
 
 
-
-
 export default app;
+
