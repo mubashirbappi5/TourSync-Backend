2b35a3053a1f6c3ed6a3b27d6b987bf4aed3d085
mubashirbappi <mubashirbappi@gmail.com>
2026-02-24 20:28:25 +0600

    error handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
 import dotenv from "dotenv";
@@ -20,6 +20,11 @@ app.get("/",(req:Request, res:Response)=>{
     res.send("Welcome to TourSync API");
 })
 
+app.use((err: Error, req: Request,res: Response, next: NextFunction)=>{
+    console.error(err.stack);
+    res.status(500).json({message:"Internal Server Error",error:err})
+    next(err);
+})
 
 
 export default app;
